cf5ff097c668dfdda83f5d084985992487e45dc9
itprogaur <professionalcodehub@gmail.com>
2022-01-16 22:35:47 +0530

    Navigation Added

---
+++ src/Components/Header.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Component.styles.css";
+
+const Header = () => {
+  return (
+    <div className="container-fluid header-title-section">
+      <div className="container justify-content-center d-flex">
+        <div className=" col-md-10 text-center">
+          <div className="mbr-section-btn ">
+            <a className="btn btn-white-outline display-4">STOCKES</a>
+            <a className="btn btn-white-outline display-4">STOCKES</a>
+            <a className="btn btn-white-outline display-4">STOCKES</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/Components/Navigation.tsx
@@ -0,0 +1,93 @@
+import React from "react";
+import "./Component.styles.css";
+import Logo from "../Common/images/tslogo-dark.png";
+
+const Navigation = () => {
+ 
+  return (
+    <div className="container-fluid">
+      <div className="container">
+        <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+          <div className="container">
+            <a className="navbar-brand" href="#">
+              <img src={Logo} id="main-logo" alt="thinksabio-logo" />
+            </a>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+              <span className="line"></span>
+              <span className="line"></span>
+              <span className="line"></span>
+            </button>
+            <div
+              className="collapse navbar-collapse"
+              id="navbarSupportedContent"
+            >
+              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                <li className="nav-item">
+                  <a className="nav-link active" aria-current="page" href="#">
+                    Home
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
+                    Link
+                  </a>
+                </li>
+                <li className="nav-item dropdown">
+                  <a
+                    className="nav-link dropdown-toggle"
+                    href="#"
+                    id="navbarDropdown"
+                    role="button"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                  >
+                    Dropdown
+                  </a>
+                  <ul
+                    className="dropdown-menu"
+                    aria-labelledby="navbarDropdown"
+                  >
+                    <li>
+                      <a className="dropdown-item" href="#">
+                        Action
+                      </a>
+                    </li>
+                    <li>
+                      <a className="dropdown-item" href="#">
+                        Another action
+                      </a>
+                    </li>
+                    <li>
+                      <hr className="dropdown-divider" />
+                    </li>
+                    <li>
+                      <a className="dropdown-item" href="#">
+                        Something else here
+                      </a>
+                    </li>
+                  </ul>
+                </li>
+              </ul>
+              <form className="d-flex">
+                <button className="btn" id="donate_button" type="submit">
+                  Search
+                </button>
+              </form>
+            </div>
+          </div>
+        </nav>
+      </div>
+    </div>
+  );
+};
+
+export default Navigation;
